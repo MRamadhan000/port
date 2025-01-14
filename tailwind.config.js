8b3db5a69a054f5daf575ab8b89ce7192d6c8a2b
MRamadhan000 <mcahya830@gmail.com>
2025-01-14 23:24:10 +0700

    fefe

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
   content: ['index.html'],
   theme: {
     extend: {
+      screens: {
+        'mobile-M': '375px',
+      },
       colors: {
         'primary': '#14b8a6',
         'dark': '#0f172a',
-      },
-      screens: {
-        'mobile-M': '375px',
       },
     },
   },
