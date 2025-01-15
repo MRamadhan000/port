7f2fe2abc9e376106b74284495e28106f8083275
MRamadhan000 <mcahya830@gmail.com>
2025-01-15 19:55:47 +0700

    final for phase 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container:{
+      center: true,
+      padding: '16px'
+    },
     extend: {
       screens: {
         'mobile-M': '375px',
